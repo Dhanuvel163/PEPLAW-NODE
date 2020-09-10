70632549ca07e13ae23e90486936f0bf41c60666
Dhanuvel163 <dhanuram99@gmail.com>
2020-09-10 23:19:10 +0530

    first

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const config = require('./config');
+
+const app = express();
+
+mongoose.connect(config.database, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+}, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Connected to the database');
+  }
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+app.use(cors());
+
+const userRoutes = require('./routes/useraccount');
+const lawyerRoutes = require('./routes/lawyeraccount');
+
+app.use('/api/useraccounts', userRoutes);
+app.use('/api/lawyeraccounts', lawyerRoutes)
+
+
+app.listen(config.port, err => {
+  console.log('Listening at port' + config.port);
+});
